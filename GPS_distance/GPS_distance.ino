258fb0927f49cc83cc61d0f8f7c44be55a76173e
Gabriel Cronin <gcronin@seattleacademy.org>
2014-04-19 12:50:09 -0700

    Integrated LCD and Keypad
    testing floating point to show it works

--- GPS_distance/GPS_distance.ino
+++ GPS_distance/GPS_distance.ino
@@ -1,3 +1,39 @@
+#include <LiquidCrystal.h>
+#include <Servo.h>
+#include <Wire.h> 
+ 
+/////////////////////////////////////////////////////KEYPAD SETUP/////////////////////////////////////////////////////// 
+#define PCA9555 0x20 // address for PCA9555
+#define OUT_P0 0x02 // Write Output port0
+#define CONFIG_P0 0x06 // Configuration port0 configures the direction of the I/O pins, 0 is output, 1 is input
+#define IN_P0 0x00 //Read Input port0
+ 
+#if defined(ARDUINO) && ARDUINO >= 100
+#define printIIC(args) Wire.write((uint8_t)args)
+#define readIIC() Wire.read()
+#else
+#define printIIC(args) Wire.send(args)
+#define readIIC() Wire.receive()
+#endif
+///////////////////////////////////////////////////END KEYPAD SETUP///////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////LCD SETUP//////////////////////////////////////////////////////////////
+LiquidCrystal lcd(A5, A4, A0, A3, A2, A1);
+boolean displayTime = false;
+const int gpsCoordinateInputBufferSize = 10;
+char input[gpsCoordinateInputBufferSize]; //buffer for input characters
+int lcdcolumnindexrow1 = 0; // used to indicate location of input key
+////////////////////////////////////////////////END LCD SETUP///////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////VARIABLE SETUP//////////////////////////////////////////////////////////
+boolean SETUP = false;
+boolean SETUPLATITUDE = false;
+boolean SETUPLONGITUDE = false; 
+uint32_t latitude, longitude, altitude, targetLatitude, targetLongitude;
+///////////////////////////////////////////////END VARIABLE SETUP//////////////////////////////////////////////////////////
+
+
 float distance_between(float lat1, float long1, float lat2, float long2, float units_per_meter) {
 	// returns distance in meters between two positions, both specified
 	// as signed decimal-degrees latitude and longitude. Uses great-circle
@@ -21,35 +57,182 @@ float distance_between(float lat1, float long1, float lat2, float long2, float u
   return delta * 6372795 * units_per_meter;
 }
 
-uint32_t latitude, longitude;
-char answer[9];
 
-void setup(){
-  Serial.begin(38400);}
+
+void setup()  
+{
+  //KEYPAD SETUP
+  Wire.begin(PCA9555); // join i2c bus (address optional for master) tried to get working
+  write_io (CONFIG_P0, B00001111); //define port 0.7-0.4 as output, 0.3-0.0 as input
   
+  //LCD SETUP
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  lcd.print("Press A for GPS");
+  lcd.setCursor(0,1);
+  lcd.print("Coordinate Setup");
+
+  //SERIAL TO COMPUTER
+  Serial.begin(38400);
+
+}
+
 void loop()
 {
-  latitude = 47367537;
-  longitude = 122189949;
-
-       
-  int nondecimalLatitude = longitude/1000000;
-  int decimalLatitude = (float(longitude - (longitude/1000000)*1000000))/60;
-  String decimalLat = "";
-  decimalLat += nondecimalLatitude;
-  decimalLat += ".";
-  decimalLat += decimalLatitude;
-  Serial.println(decimalLat);
-  
-  for(int i = 0; i<9; i++) {answer[i] = decimalLat.charAt(i); }
-  Serial.println(answer);
-  float answer2 = atof(answer);
- Serial.println(answer2, 4); 
-  delay(2000);
-  float lat1 = 47.6133;
-  float lat2 = 47.6233;
-  float long1 = 122.3157;
-  float long2 = 122.3133;
-  Serial.println(distance_between(lat1, long1, lat2, long2, 1.0));
-  delay(2000);
+  unsigned char key;                                 
+  key=readdata();
+  while (readdata()==key && key!= 'E')  {}  //keypad debounce
+  if(key == 'B') {}//displayTime = false; } // Display GPS mode
+  else if(key == 'C') {}//displayTime = true; } // Display Time mode
+  else if(key == 'A')  // Check for setup mode
+  {   
+    lcd.clear();
+    lcd.print("Enter Latitude");
+    lcd.setCursor(0,1);
+    lcd.print("ddmmmmmm");
+    SETUPLATITUDE = true;
+    for(int i = 0; i < gpsCoordinateInputBufferSize; i++) { input[i] = '0'; }  // initialize input as zero
+    lcdcolumnindexrow1 = 0;
+  }
+  else if(SETUPLATITUDE)  // user input of latitude
+  {
+    if(key != 'E') {// received a keystroke
+        if(key == 'D') {
+          SETUPLATITUDE = false;
+          SETUPLONGITUDE = true; 
+          targetLatitude = atol(input);
+          Serial.println(targetLatitude);
+          lcd.clear();
+          lcd.print("Enter Longitude");
+          lcd.setCursor(0,1);
+          lcd.print("dddmmmmmm");
+          for(int i = 0; i < gpsCoordinateInputBufferSize; i++) { input[i] = '0'; }  // initialize input as zero
+          lcdcolumnindexrow1 = 0;
+         }
+        else if(key != 'B' && key!= 'C' && key != '#' && key != '*' && key != 'A') {        
+          for(int i =0; i < (gpsCoordinateInputBufferSize-1); i++) {input[i] = input[i+1]; }  //shift all the values left
+          input[(gpsCoordinateInputBufferSize-1)] = key;
+          lcd.setCursor(lcdcolumnindexrow1,1);  //print on left of LCD moving right
+          lcd.print(input[(gpsCoordinateInputBufferSize-1)]);
+          ++lcdcolumnindexrow1;
+        }
+    }
+  }
+  else if(SETUPLONGITUDE)  // user input of longitude
+  {
+    if(key != 'E') {// received a keystroke
+        if(key == 'D') {
+          SETUPLONGITUDE = false;
+          SETUP = true; 
+          targetLongitude = atol(input);
+          Serial.println(targetLongitude);
+          }
+        else if(key != 'B' && key!= 'C' && key != '#' && key != '*' && key != 'A') {        
+          for(int i =0; i < (gpsCoordinateInputBufferSize-1); i++) {input[i] = input[i+1]; }  //shift all the values left
+          input[(gpsCoordinateInputBufferSize-1)] = key;
+          lcd.setCursor(lcdcolumnindexrow1,1);  //print on left of LCD moving right
+          lcd.print(input[(gpsCoordinateInputBufferSize-1)]);
+          ++lcdcolumnindexrow1;
+        }
+    }
+  }
+  else if(!SETUP) { }  // infinite loop for the very beginner so it keeps displaying setup prompt
+  else {
+    
+  float latitudeDecimal = latitude;
+  float longitudeDecimal = longitude;
+  latitudeDecimal /= 1000000;
+  longitudeDecimal /=1000000;
+  Serial.println(latitudeDecimal, 6);
+  Serial.println(longitudeDecimal, 6);
+  delay(3000);
+  }
 }
+
+
+//////////////////////////////////////////////////////////////////KEYPAD FUNCTIONS//////////////////////////////////////////////////////////////
+unsigned char readdata(void)          //main read function
+{
+                       //binary 10000000, set pin 0.7 HIGH
+  for (int i=0;i<4;i++)               //for loop
+  {
+    int input=0xF0; 
+    input ^= (1 << (i+4));
+    write_io (OUT_P0, input);
+      
+    for (int j=0;j<4;j++)
+      {  
+        unsigned int temp=0x0F;            //temporary integer, binary 1111, to compare with gpio_read() function and determine pression 
+        temp ^= (1 << j);  
+        //Serial.println(gpio_read(PCA9555));
+        if (gpio_read(PCA9555)==temp)
+         { return outputchar(i,j);}   // output the char
+      }
+    input=input>>1;                   //shift right, set the next pin HIGH, set previous one LOW
+  }
+  return 'E';                         // if no button is pressed, return E
+}
+  
+ 
+ void write_io(int command, int value)  //write into register
+{
+  Wire.beginTransmission(PCA9555);      //begin transmission  
+  printIIC(command);                  //which register
+  printIIC(value);                    //write value
+  Wire.endTransmission();               //end transmission
+}
+ 
+unsigned int gpio_read(int address)   //read from pin 0.3 ~ 0.0
+{
+  int data = 0;                   
+  Wire.beginTransmission(address);
+  printIIC(IN_P0);                // warning: this may be a bug in Arduino 1.0. transform 0x00 (input register) into byte 0, otherwise this compiler will return error
+  Wire.endTransmission();
+  Wire.beginTransmission(address);
+  Wire.requestFrom(address, 1);
+  if (Wire.available()) 
+  {   
+    data = readIIC( )&0x0F ;           // read lower 4 bit  0.3 ~ 0.0
+        
+  }
+  Wire.endTransmission();
+  return data;
+}
+ 
+ 
+unsigned char outputchar(int i, int j)   // output chars on the pad
+{
+  if (i==0)
+  { switch (j)
+    {case 0:           return '1'; break;
+     case 1:           return '2'; break;
+     case 2:           return '3'; break;
+     case 3:           return 'A'; break;
+    }
+  }
+   if (i==1)
+  { switch (j)
+    {case 0:           return '4'; break;
+     case 1:           return '5'; break;
+     case 2:           return '6'; break;
+     case 3:           return 'B'; break;
+    }
+  }
+   if (i==2)
+  { switch (j)
+    {case 0:           return '7'; break;
+     case 1:           return '8'; break;
+     case 2:           return '9'; break;
+     case 3:           return 'C'; break;
+    }
+  }
+   if (i==3)
+  { switch (j)
+    {case 0:           return '*'; break;
+     case 1:           return '0'; break;
+     case 2:           return '#'; break;
+     case 3:           return 'D'; break;
+    }
+  }
+}
+//////////////////////////////////////////////////////////////////END KEYPAD FUNCTIONS//////////////////////////////////////////////////////////////
